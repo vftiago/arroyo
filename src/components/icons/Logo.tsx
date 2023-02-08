5f548f8896e0e31452e5c9b4720eafa6cfe3e493
tiago <vftiago@gmail.com>
2023-02-08 20:24:05 +0000

    💄 update logo .svg

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 
-const Icon = ({ size = 16 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    data-name="Layer 1"
-    viewBox="0 0 142.93 223.2"
-    width={size}
-    height={size}
-  >
-    <path
-      d="M783.84 762.72c-9.32 0-18.38.89-27.19-.22C744.87 761 737 751.6 737 739.67q-.23-98.7 0-197.41a14 14 0 01.31-1.85h30.83v191.47h15.77c-.07 10.58-.07 20.64-.07 30.84zM832.88 762.87v-196.1c0-16.95 9.73-26.74 26.57-26.77h20.35v31.2h-15.59v164.95c0 18.85-11.68 29.04-31.33 26.72z"
-      transform="translate(-736.88 -540)"
-    ></path>
-  </svg>
-);
+function Logo({ size = 16 }) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 136 192"
+			width={size}
+			height={size}
+		>
+			<g className="cls-1" data-name="Rectangle 2">
+				<path
+					d="M60 0H28v171.43C28 182.79 38.74 192 52 192h8V0z"
+					className="cls-2"
+					data-name="Rectangle 2"
+					transform="translate(-28)"
+				></path>
+			</g>
+			<g className="cls-1" data-name="Rectangle 2">
+				<g data-name="Rectangle 2">
+					<path d="M128 0H136V24H128z" className="cls-2"></path>
+					<path
+						d="M136 0L128 0 128 24 136 24 136 0 136 0z"
+						className="cls-2"
+					></path>
+				</g>
+			</g>
+			<g className="cls-1" data-name="Rectangle 2">
+				<path
+					d="M156 0h-8c-13.26 0-24 9.21-24 20.57V192h8c13.26 0 24-9.21 24-20.57V0z"
+					className="cls-2"
+					data-name="Rectangle 2"
+					transform="translate(-28)"
+				></path>
+			</g>
+		</svg>
+	);
+}
 
-export default Icon;
+export default Logo;
